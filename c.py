4e79abff6dfd85c7f85e1f2cd6bfcd2a16313433
Jinhong1003 <54864182+Jinhong1003@users.noreply.github.com>
2020-09-21 15:46:16 -0700

    Add files via upload

---
+++ c.py
@@ -0,0 +1,8 @@
+import a
+import b
+import matplotlib.pyplot as plt
+plt.plot(a.l1,b.l2,'s-',color = 'r',label="Visualization")
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+plt.legend(loc = "best")
+plt.show()
